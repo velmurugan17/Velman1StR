5bba9e02073a020f2eef3b4cca2ac17bc5bda0c9
velmurugan17 <velmurugan.sw@gmail.com>
2018-10-11 16:49:03 +0530

    Update webhookCollect.py

--- webhookCollect.py
+++ webhookCollect.py
@@ -1,3 +1,7 @@
+"""
+This web api listens for shopify webhooks,parses any order based incoming webhooks and pushes the data to redshift.
+"""
+
 from flask import Flask, request, Response
 from lib import table_name, push_data_to_redshift
 import json
@@ -7,6 +11,9 @@ app = Flask(__name__)
 
 @app.route('/shopify', methods=['POST'])
 def api_echo():
+    """
+    parses the post request and pushes to redshift
+    """
     if request.headers['Content-Type'] == 'application/json':
         wh_data = [json.dumps(request.json)]
         push_data_to_redshift(wh_data, table_name)
